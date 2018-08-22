c0972e4b3ae366175294add2e1e93def34b66b96
Xiaoting Zhong <xiaotinz@andrew.cmu.edu>
2018-08-21 20:18:01 -0400

    working

--- Grain Tracking/heightUsingTruncatedConeModel.m
+++ Grain Tracking/heightUsingTruncatedConeModel.m
@@ -2,16 +2,14 @@ function h = heightUsingTruncatedConeModel(area1, area2, v)
 % ############################################################################
 % Given two surface areas and the volume, calculate heigt as if the
 % geometry is truncated cone
+% See http://mathworld.wolfram.com/PyramidalFrustum.html
 % ############################################################################
 % ----------------------- load debug data -----------------------
 % area1 = 16;
 % area2 = 23.309;
 % v = 25.9274;
 % ---------------------------------------------------------------
-r1 = sqrt(area1/pi);
-r2 = sqrt(area2/pi);
-
-h = 3*v/(pi*(r1*r2 + r1^2 + r2^2));
+    h = 3*v/(area1 + area2 + sqrt(area1*area2));
 end
 
 
